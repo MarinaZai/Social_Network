415f3b025d55b135221eb4f24411edaa3ea7b3f5
MarinaZai <Marintos34@gmail.com>
2022-05-18 11:51:53 +0300

    kf

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App(props) {
         <Navbar />
         <div className='app-wrapper-content'>
           <Routes>
-            <Route path="/dialog" element={<Dialogs />} />
+            <Route path="/dialogs" element={<Dialogs />} />
             <Route path="/profile" element={<Profile />} />
           </Routes>
         </div>
--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import s from './navbar.module.css'
+import s from './navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 
-  const Navbar = () => {
+const Navbar = () => {
   return (
     <nav className={s.nav}>
-      <div className={`${s.item} ${s.active}`}><a href="/profile">Profile</a></div>
-      <div className={s.item}><a href="/dialogs">Messages</a></div>
-      <div className={s.item}><a href="/News">News</a></div>
-      <div className={s.item}><a href="/Music">Music</a></div>
-      <div className={s.item}><a href="/Settings">Settings</a></div>
+      <div className={s.item}>
+        <NavLink to="/profile" className = { navData => navData.isActive ? s.active : s.item }>Profile</NavLink>
+      </div>
+      <div className={`${s.item}  ${s.active}`}>
+        <NavLink to="/dialogs" className = { navData => navData.isActive ? s.active : s.item }>Messages</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/News">News</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/Music">Music</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/Settings">Settings</NavLink>
+      </div>
     </nav>
   )
 }
